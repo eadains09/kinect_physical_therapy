3886ffbdd215d9cac5ab9703dd98f79dd17f26e3
eadains09 <eadains09@gmail.com>
2015-10-23 17:40:42 -0700

    changing name of window

--- PT@Home/Display.cpp
+++ PT@Home/Display.cpp
@@ -106,7 +106,7 @@ bool Display::init() {
         success = false;
     } else {
         //Create window
-        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        window = SDL_CreateWindow("Kinect Display", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
         if (window == NULL) {
             printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
             success = false;
