1bd315835a17da73aa3882719f5b3bf4186a2ff2
Erika <eadains09@users.noreply.github.com>
2015-12-13 19:22:48 -0800

    Adding comments to Button class

--- PT@Home/Button.cpp
+++ PT@Home/Button.cpp
@@ -1,9 +1,13 @@
-//
-//  Button.cpp
-//
-//  Created by Erika Dains on 11/8/15.
-//  Copyright (c) 2015 Erika Dains. All rights reserved.
-//
+/*
+ *  File:              Button.cpp
+ *
+ *  Purpose:           Class to give properties to any button being rendered on screen.
+ *
+ *  Member variables:  Button type, SDL surface and texture, default button size, 
+ *                     button position in source (.bmp file) and destination (SDL_Renderer)
+ *
+ *  Created by Erika Dains on 11/8/15.
+ */
 
 #include "Button.h"
 #include "stdafx.h"
@@ -12,24 +16,12 @@
 #include "Display.h"
 
 
-/*Button::Button()
-{
-    mCurrentSprite = BUTTON_SPRITE_PLAY;
-    
-    srcR.x = 0;
-    srcR.y = 0;
-    srcR.w = BUTTON_WIDTH;
-    srcR.h = BUTTON_HEIGHT;
-    
-    destR.x = 0;
-    destR.y = 0;
-    destR.w = BUTTON_WIDTH;
-    destR.h = BUTTON_HEIGHT;
-    
-    buttonSurface = NULL;
-
-}
-*/
+// Constructor - using default button size
+/*  In vals:    type - what kind of button it is
+ *              x - x position of button on displayed screen
+ *              y - y positoin of button on displayed screen
+ *              image - name of file where button image is stored
+ */
 Button::Button(ButtonSprite type, int x, int y, std::string image) {
 	mCurrentSprite = type;
     
@@ -46,6 +38,14 @@ Button::Button(ButtonSprite type, int x, int y, std::string image) {
     buttonSurface = SDL_LoadBMP(image.c_str());
 }
 
+// Constructor - using specified button size
+/*  In vals:    type - what kind of button it is
+ *              w - width of button
+ *              h - height of button
+ *              x - x position of button on displayed screen
+ *              y - y positoin of button on displayed screen
+ *              image - name of file where button image is stored
+ */
 Button::Button(ButtonSprite type, int w, int h, int x, int y, std::string image) {
     mCurrentSprite = type;
     
@@ -71,12 +71,18 @@ ButtonSprite Button::getType() {
 	return mCurrentSprite;
 }
 
+// Sets position of button in destination (SDL_Renderer)
+// i.e. where the button will be seen on the displayed screen
 void Button::setPosition( int x, int y )
 {
     destR.x = x;
     destR.y = y;
 }
 
+/* Takes in an SDL_Event of MOUSEBUTTONDOWN, checks the position 
+ * of the click against the position of the button on the screen.
+ * Returns true if button has been clicked
+ */
 bool Button::isInside(SDL_Event *e) {
     //Get mouse position
     int x, y;
@@ -86,25 +92,21 @@ bool Button::isInside(SDL_Event *e) {
     bool inside = true;
         
     //Mouse is left of the button
-    //if( x < mPosition.x )
     if (x < destR.x)
     {
         inside = false;
     }
     //Mouse is right of the button
-    //else if( x > mPosition.x + BUTTON_WIDTH )
     else if (x > destR.x + destR.w)
     {
         inside = false;
     }
     //Mouse above the button
-    //else if( y < mPosition.y )
     else if (y < destR.y)
     {
         inside = false;
     }
     //Mouse below the button
-    //else if( y > mPosition.y + BUTTON_HEIGHT )
     else if (y > destR.y + destR.h)
     {
         inside = false;
@@ -113,15 +115,17 @@ bool Button::isInside(SDL_Event *e) {
     return inside;
 }
 
+// Renders the button on the specified renderer
+// Once texture is copied over to renderer it must be deleted, 
+// otherwise it will be a memory leak when function is called again
 void Button::render(SDL_Renderer* renderer) {
     buttonTexture = SDL_CreateTextureFromSurface(renderer, buttonSurface);
     SDL_RenderCopy(renderer, buttonTexture, NULL, &destR);
 	SDL_DestroyTexture(buttonTexture);
 }
 
+// Deletes button's Surface and Texture, should any exist
 void Button::freeButton() {
-	//if (buttonSurface != NULL) {
-		SDL_FreeSurface(buttonSurface);
-	//}
+	SDL_FreeSurface(buttonSurface);
     SDL_DestroyTexture(buttonTexture);
 }
